86646bf4eaeaa05446e9ff3a8adb3c8900293adb
trba1810 <trba8l@live.com>
2023-01-30 13:05:03 +0100

    install router dom and add two routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
